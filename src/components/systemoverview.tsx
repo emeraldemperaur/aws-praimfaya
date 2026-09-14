e74f4c419f3c04708841a4d28e06366b96731f2d
meka.egwim <egwim.emeka@gmail.com>
2026-09-13 17:18:21 -0700

    DashboardUI :: Content Import path fix v2

--- src/components/systemoverview.tsx
+++ src/components/systemoverview.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import * as d3 from 'd3';
-import '../styles/systemOverview.scss';
+import '../styles/systemoverview.scss';
 
 interface SystemOverviewProps {
     searchQuery: string;
