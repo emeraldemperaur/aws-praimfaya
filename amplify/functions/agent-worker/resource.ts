32270015e585af93c64bf4a64f682da482e9947e
meka.egwim <egwim.emeka@gmail.com>
2026-09-16 23:28:25 -0700

    Bedrock Agents :: Agent Worker Lambda Fix

--- amplify/functions/agent-worker/resource.ts
+++ amplify/functions/agent-worker/resource.ts
@@ -2,7 +2,7 @@ import { defineFunction } from '@aws-amplify/backend';
 
 export const agentWorker = defineFunction({
   name: 'agent-worker',
-  entry: './index.ts',
+  entry: './handler.ts',
   timeoutSeconds: 900, 
   resourceGroupName: 'data',
   memoryMB: 1024,
